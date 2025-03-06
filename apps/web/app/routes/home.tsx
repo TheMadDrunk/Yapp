b2ad9297bbea597c5d7b543bb6b73378f0e0884d
TheMadDrunk <hamza.alaoui@nemo.ma>
2025-03-06 10:46:26 +0000

    wip: hover text clip

--- apps/web/app/routes/home.tsx
+++ apps/web/app/routes/home.tsx
@@ -3,11 +3,14 @@ import { Button } from "../components/ui/Button";
 import { SocialLinkList } from "~/components/SocialLinkList";
 import { ArticlesCardListing, type ArticleCardProps } from "~/components";
 import type { SocialLinkProps } from "~/components/SocialLink/SocialLink";
+import { GitBranch } from "lucide-react";
 
 export async function loader() {
+
+  const githubIcon = `<svg xmlns="http://www.w3.org/2000/svg" width="48" height="48" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><path d="M15 22v-4a4.8 4.8 0 0 0-1-3.5c3 0 6-2 6-5.5.08-1.25-.27-2.48-1-3.5.28-1.15.28-2.35 0-3.5 0 0-1 0-3 1.5-2.64-.5-5.36-.5-8 0C6 2 5 2 5 2c-.3 1.15-.3 2.35 0 3.5A5.403 5.403 0 0 0 4 9c0 3.5 3 5.5 6 5.5-.39.49-.68 1.05-.85 1.65-.17.6-.22 1.23-.15 1.85v4"/><path d="M9 18c-4.51 2-5-2-7-2"/></svg>`;
   const socialLinks: SocialLinkProps[] = [
     {
-      icon: `<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><path d="M15 22v-4a4.8 4.8 0 0 0-1-3.5c3 0 6-2 6-5.5.08-1.25-.27-2.48-1-3.5.28-1.15.28-2.35 0-3.5 0 0-1 0-3 1.5-2.64-.5-5.36-.5-8 0C6 2 5 2 5 2c-.3 1.15-.3 2.35 0 3.5A5.403 5.403 0 0 0 4 9c0 3.5 3 5.5 6 5.5-.39.49-.68 1.05-.85 1.65-.17.6-.22 1.23-.15 1.85v4"/><path d="M9 18c-4.51 2-5-2-7-2"/></svg>`,
+      icon: githubIcon.replace('width="48"', "width='24'").replace('height="48"', "height='24'"),
       name: "GitHub",
       href: "https://github.com/YOUR_USERNAME",
     },
@@ -50,7 +53,82 @@ export async function loader() {
     },
   ];
 
-  return { socialLinks, articles };
+
+  const skills = [
+    {
+      name: "React",
+      icon: githubIcon,
+    },
+    {
+      name: "React",
+      icon: githubIcon,
+    },
+    {
+      name: "React",
+      icon: githubIcon,
+    },
+    {
+      name: "React",
+      icon: githubIcon,
+    },
+    {
+      name: "React",
+      icon: githubIcon,
+    },
+    {
+      name: "React",
+      icon: githubIcon,
+    },
+    {
+      name: "React",
+      icon: githubIcon,
+    },
+    {
+      name: "React",
+      icon: githubIcon,
+    },
+    {
+      name: "React",
+      icon: githubIcon,
+    },
+    {
+      name: "React",
+      icon: githubIcon,
+    },
+    {
+      name: "React",
+      icon: githubIcon,
+    },
+    {
+      name: "React",
+      icon: githubIcon,
+    },
+    {
+      name: "React",
+      icon: githubIcon,
+    },
+    {
+      name: "React",
+      icon: githubIcon,
+    },
+    {
+      name: "React",
+      icon: githubIcon,
+    },
+    {
+      name: "React",
+      icon: githubIcon,
+    },
+    {
+      name: "React",
+      icon: githubIcon,
+    },
+    {
+      name: "React",
+      icon: githubIcon,
+    },
+  ];
+  return { socialLinks, articles, skills };
 }
 
 export function meta({ }: Route.MetaArgs) {
@@ -61,7 +139,7 @@ export function meta({ }: Route.MetaArgs) {
 }
 
 export default function Home({ loaderData }: Route.ComponentProps) {
-  const { socialLinks, articles } = loaderData;
+  const { socialLinks, articles, skills } = loaderData;
   console.log("[socialLinks]", socialLinks);
   return (
     <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
@@ -86,6 +164,31 @@ export default function Home({ loaderData }: Route.ComponentProps) {
 
       <SocialLinkList links={socialLinks} />
       <ArticlesCardListing articles={articles} />
+
+      <div className="mt-10 grid grid-cols-7 grid-rows-6 ">
+        <h2 className="text-lg text-primary font-semibold col-span-1">./About me</h2>
+        <p className="text-primary max-w-xl text-justify col-span-3 col-start-5 italic">
+          &quot;Hi, I'm TMD. Software Engineer alumnus of University of California, Los Angeles.
+          Passionate from my young age about programming and problem solving.&quot;
+        </p>
+        <div className="col-span-3 col-start-5 row-start-2 row-span-5 flex justify-center items-center sepia-100">
+          <img src="https://picsum.photos/400/600" alt="TMD" />
+        </div>
+        <div className="w-[11rem] h-30 p-1">
+          <div className="w-full h-full bg-primary text-background"> My Skills</div>
+        </div>
+        {skills.map((skill) => (
+          <div className="w-[11rem] h-30 p-1">
+            <div className="w-full h-full flex justify-center items-center bg-clip-text hover:bg-primary hover:text-background transition-all duration-300 text-transparent" >
+              {skill.name}
+            </div>
+          </div>
+        ))}
+        <div className="w-[11rem] h-30 p-1">
+          <div className="w-full h-full bg-primary text-background"> And More...</div>
+        </div>
+
+      </div>
     </div>
   );
 }
