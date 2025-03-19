95c231266100ce06f58c2e4cdda71ce9417d8033
TheMadDrunk <hamza.alaoui@nemo.ma>
2025-03-19 23:56:55 +0000

    feat: Add Docker support and environment variable management; update README with setup instructions

--- apps/web/app/config/env.ts
+++ apps/web/app/config/env.ts
@@ -1,7 +1,11 @@
 const env = {
-    STRAPI_TOKEN: "5989f44df3e53bc2fc31c4b9e4a47bebbdf91d422b33b54767fc1a6365be880d8c174fa96caebbd840274edd28a744844a1d1becc1539b00c02c94a6dfe005fc1c96cdbee1313d5268bb4daff7cc5e9f5ea343688ce90ca127ad0bbbef20378ec6d825a046e3662ebfd21a7c20f2b8655f2abb9f1e12666984d006b3e35945a7",
-    STRAPI_URL: "http://localhost:1337",
-    NODE_ENV: "dev",
+    STRAPI_TOKEN: process.env.STRAPI_TOKEN || '',
+    STRAPI_URL: process.env.STRAPI_URL || 'http://localhost:1337',
+    NODE_ENV: process.env.NODE_ENV || 'development',
 };
+
+if (!env.STRAPI_TOKEN) {
+    throw new Error('STRAPI_TOKEN is required');
+}
 
 export default env;
